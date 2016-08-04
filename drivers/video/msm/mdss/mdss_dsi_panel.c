e577bd994dd3dfa3923019baeb257d5f41e329d3
Steve Kondik <steve@cyngn.com>
2016-08-04 10:02:10 -0400

    video: mdss: Improve the LiveDisplay driver
     * Use a workqueue to apply changes to the hardware to reduce delays
     * Handle all update modes consistently
     * Simplify locking to prevent race conditions (seen when setting
       gamma presets)
    
    Change-Id: Ide8af437e1143a27f5b51325c27051dd97cda6b0

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,8 +26,6 @@
 #include "mdss_dba_utils.h"
 #include "mdss_livedisplay.h"
 
-#include "mdss_livedisplay.h"
-
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -614,12 +612,6 @@ static int mdss_dsi_set_col_page_addr(struct mdss_panel_data *pdata,
 			}
 		}
 	}
-
-#ifdef CONFIG_YULONG_COLOR
-	color_enhancement_impl_apply();
-#endif
-
-	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
 
 end:
 	return 0;
@@ -1706,8 +1698,6 @@ static int mdss_dsi_parse_panel_features(struct device_node *np,
 			pr_err("%s:%d, Disp_en gpio not specified\n",
 					__func__, __LINE__);
 	}
-
-	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 }
--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -552,8 +552,6 @@ struct mdss_panel_info {
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
-
-	struct mdss_livedisplay_ctx *livedisplay;
 };
 
 struct mdss_panel_data {
