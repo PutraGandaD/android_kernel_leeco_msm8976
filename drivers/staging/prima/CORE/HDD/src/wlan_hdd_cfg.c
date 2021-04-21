2f2e6ebd70594a10ab36d775ede84050f5d52d73
PutraGandaD <putraganda.dewata@gmail.com>
2021-04-22 01:56:17 +0700

    prima: disable qcom_rx_wakelock

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg.c
@@ -206,9 +206,7 @@ REG_TABLE_ENTRY g_registry_table[] =
    REG_VARIABLE( CFG_RTS_THRESHOLD_NAME, WLAN_PARAM_Integer,
                  hdd_config_t, RTSThreshold,
                  VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
-                 CFG_RTS_THRESHOLD_DEFAULT,
-                 CFG_RTS_THRESHOLD_MIN,
-                 CFG_RTS_THRESHOLD_MAX ),
+                 0, 0, 0),
 
    REG_VARIABLE(CFG_MARK_INDOOR_AS_DISABLE_NAME,
                  WLAN_PARAM_Integer,
